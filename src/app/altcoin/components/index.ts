9ca6dc4581634ba236440a41dafbafbec9429318
Vasil Folizyuk <vasyafoxm@gmail.com>
2017-10-30 22:14:40 +0200

    * CLOUT-61 fix chart add side bars

--- src/app/altcoin/components/index.ts
+++ src/app/altcoin/components/index.ts
@@ -2,6 +2,7 @@ export * from './altcoin-list/altcoin-list.component';
 export * from './altcoin-detail/altcoin-detail.component';
 export * from './altcoin-detail/feature-detail/feature-detail.component';
 export * from './altcoin-detail/feature-tabs/feature-tabs.component';
+export * from './altcoin-detail/feature-tabs/chart-tab/chart-tab.component';
 export * from './altcoin-detail/feature-comment-list/feature-comment-list.component';
 export * from './altcoin-detail/feature-comment-list/feature-comment-item/feature-comment-item.component';
 export * from './altcoin-list/altcoin-item/altcoin-item.component';
