74e18fa2395da0794505613649b7785e0e8c4dfa
alex <a.vorobyov@bvblogic.com>
2017-11-04 00:38:21 +0200

    * disabled fetching history.

--- config/schedule.js
+++ config/schedule.js
@@ -3,34 +3,34 @@
  */
 module.exports.schedule = {
   tasks: {
-    syncAltcoinHystory: {
-      cron: "*/2 * * * *",
-      task: function () {
-        Altcoin.findOne({history_sync: {not: true}}).exec(function (err, altcoin) {
-          if (err) return err;
-          CoinMarketCap.getHistory(altcoin.id).then(function (data) {
-            for (var key in data.price_usd) {
-              AltcoinPrice.findOrCreate({
-                altcoin: altcoin.id,
-                timestamp: data.price_usd[key][0],
-              }, {
-                altcoin: altcoin.id,
-                timestamp: data.price_usd[key][0],
-                price_usd: data.price_usd[key][1],
-                volume_usd: data.volume_usd[key][1],
-                price_btc: data.price_btc[key][1],
-                market_cap_by_available_supply: data.market_cap_by_available_supply[key][1]
-              }).exec(function createFindCB(error, createdOrFoundRecords) {
-
-
-              })
-            }
-            altcoin.history_sync = true;
-            altcoin.save();
-          })
-        })
-      }
-    },
+    // syncAltcoinHystory: {
+    //   cron: "*/2 * * * *",
+    //   task: function () {
+    //     Altcoin.findOne({history_sync: {not: true}}).exec(function (err, altcoin) {
+    //       if (err) return err;
+    //       CoinMarketCap.getHistory(altcoin.id).then(function (data) {
+    //         for (var key in data.price_usd) {
+    //           AltcoinPrice.findOrCreate({
+    //             altcoin: altcoin.id,
+    //             timestamp: data.price_usd[key][0],
+    //           }, {
+    //             altcoin: altcoin.id,
+    //             timestamp: data.price_usd[key][0],
+    //             price_usd: data.price_usd[key][1],
+    //             volume_usd: data.volume_usd[key][1],
+    //             price_btc: data.price_btc[key][1],
+    //             market_cap_by_available_supply: data.market_cap_by_available_supply[key][1]
+    //           }).exec(function createFindCB(error, createdOrFoundRecords) {
+    //
+    //
+    //           })
+    //         }
+    //         altcoin.history_sync = true;
+    //         altcoin.save();
+    //       })
+    //     })
+    //   }
+    // },
     syncAltcoin: {
       cron: "*/30 * * * *",
       task: function () {
