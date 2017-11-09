d93db73827642b075d332c65a7968d1305f07b90
Alexandr Vorobyov <a.vorobyov@bvblogic.com>
2017-11-09 15:00:07 +0200

    * Url og Info.

---
+++ api/controllers/UrlController.js
@@ -0,0 +1,20 @@
+/**
+ * UrlController
+ *
+ * @description :: Server-side logic for managing Urls
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+const ogs = require('open-graph-scraper');
+
+module.exports = {
+  ogInfo: function (req, res) {
+    var url = req.param("url");
+    const options = {'url': url};
+    ogs(options, function (err, results) {
+      if (err) return res.json(400, err);
+      return res.json(results)
+    });
+  }
+};
+
--- config/routes.js
+++ config/routes.js
@@ -628,6 +628,36 @@ var routes = [
     }
   },
 
+  {
+    method: "GET", path: "/url/og", target:
+    {
+      controller: 'UrlController',
+      action: 'ogInfo',
+      skipAssets: 'true',
+      swagger: {
+        methods: ['GET'],
+        summary: 'Get og info',
+        description: 'Get og info',
+        produces: [
+          'application/json'
+        ],
+        tags: [
+          'Url'
+        ],
+        responses: {
+          '200': {
+            description: 'Og info about url'
+          }
+        },
+        parameters: [
+          {in: "query", name: "url"}
+        ]
+
+
+      }
+    }
+  },
+
   {method: "GET", path: "/altcoins/sync", target: "AltcoinController.sync"},
   {method: "GET", path: "/icos/sync/:type", target: "IcoController.sync"},
   {method: "GET", path: "/icos/syncphoto/", target: "IcoController.syncPhoto"},
