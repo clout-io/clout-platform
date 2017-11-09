6a54df9a90637de9b488910dc72c4c9f5f66ee27
alex <a.vorobyov@bvblogic.com>
2017-11-09 20:35:18 +0200

    * CLOUT-123 Add Functionality of Saving Links to Posts

--- api/models/Post.js
+++ api/models/Post.js
@@ -5,6 +5,7 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 var cryptoRandomString = require('crypto-random-string');
+const ogs = require('open-graph-scraper');
 
 module.exports = {
 
@@ -25,11 +26,14 @@ module.exports = {
       type: "string",
       url: true
     },
-    "link": {
+    link: {
       type: "string",
       url: true
     },
-    "attachment": {
+    linkData: {
+      type: "json"
+    },
+    attachment: {
       collection: "Img"
     },
     owner: {
@@ -40,7 +44,32 @@ module.exports = {
       type: "integer",
       defaultsTo: 0
     }
+  },
 
+  beforeCreate: function (values, next) {
+    if (values.link) {
+      var url = values.link;
+      const options = {'url': url};
+      ogs(options, function (err, results) {
+        values.linkData = results.data;
+        next()
+      });
+    } else {
+      next()
+    }
+  },
+
+  beforeUpdate: function (values, next) {
+    if (values.link) {
+      var url = values.link;
+      const options = {'url': url};
+      ogs(options, function (err, results) {
+        values.linkData = results.data;
+        next()
+      });
+    } else {
+      next()
+    }
   }
 };
 
