5eff757286d362d95d262b528a5166d097b1c6c6
Vasil Folizyuk <vasyafoxm@gmail.com>
2017-12-01 18:03:03 +0200

    * CLOUT-177 add fixed position for sidebars on all pages

--- src/app/feed/components/news/news.component.ts
+++ src/app/feed/components/news/news.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 declare const $: any;
 
@@ -6,24 +6,18 @@ declare const $: any;
   selector: 'app-news',
   templateUrl: './news.component.html'
 })
-export class NewsComponent implements OnInit, AfterViewInit, OnDestroy {
+export class NewsComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() { }
-
   ngAfterViewInit() {
-    const me = this;
-
-    if (window.matchMedia("(min-width: 992px)").matches) {
+    if (window.matchMedia('(min-width: 992px)').matches) {
       $('.feed-aside__item').stick_in_parent({
         offset_top: 20,
         parent: '.feed-content',
         recalc_every: 4
-      })
+      });
     }
-
   }
 
-  ngOnDestroy() { }
 }
