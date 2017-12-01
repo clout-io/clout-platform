843b2438e19d592380ee6a37d25b744ce091849d
Igor Pylypko <igor.pylypko@gmail.com>
2017-12-01 12:50:20 +0200

    Fixed Sidebars

--- src/app/feed/components/news/news.component.ts
+++ src/app/feed/components/news/news.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
+declare const $: any;
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html'
 })
-export class NewsComponent implements OnInit, OnDestroy {
+export class NewsComponent implements OnInit, AfterViewInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit() { }
+
+  ngAfterViewInit() {
+    const me = this;
+
+    if (window.matchMedia("(min-width: 992px)").matches) {
+      $('.feed-aside__item').stick_in_parent({
+        offset_top: 20,
+        parent: '.feed-content'
+      })
+    }
+    
+  }
 
   ngOnDestroy() { }
 }
