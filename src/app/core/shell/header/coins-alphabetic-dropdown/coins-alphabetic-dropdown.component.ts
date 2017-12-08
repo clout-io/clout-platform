659673c3ac092979177f958b00fccebdbf8d8261
Vasil Folizyuk <vasyafoxm@gmail.com>
2017-12-08 17:17:34 +0200

    * CLOUT-193 add alpabetic coins dropdown

---
+++ src/app/core/shell/header/coins-alphabetic-dropdown/coins-alphabetic-dropdown.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FeedService } from '../../../../services';
+
+import { sortBy, compose, prop, toLower } from 'ramda';
+declare const $: any;
+
+@Component({
+  selector: 'app-coins-alphabetic-dropdown',
+  templateUrl: './coins-alphabetic-dropdown.component.html',
+  styleUrls: ['./coins-alphabetic-dropdown.component.scss']
+})
+export class CoinsAlphabeticDropdownComponent implements OnInit {
+  public alphabeticList = [];
+  public filterValue: string;
+
+  constructor(private router: Router,
+              private feedService: FeedService) { }
+
+  ngOnInit() {
+    const url = this.router.url;
+    if (url === '/icos') {
+      this.feedService.getIcosAlphabetic().take(1)
+        .subscribe(data => this.initDropdown(data));
+    } else {
+      this.feedService.getAltcoinsAlphabetic().take(1)
+        .subscribe(data => this.initDropdown(data));
+    }
+  }
+
+  initDropdown(data: any): void {
+    this.alphabeticList = this.parseList(data);
+    this.addScrollToDropdown();
+  }
+
+  parseList(data) {
+    const list = [];
+    for (const i in data) {
+      list.push({
+        groupName: i,
+        items: data[i]
+      });
+    }
+
+    const sortByGroupName = sortBy(compose(toLower, prop('groupName')));
+    return sortByGroupName(list);
+  }
+
+  addScrollToDropdown(): void {
+    $('.coins-alphabetical-dropdown-list').mCustomScrollbar({
+      scrollInertia: 200,
+      mouseWheel: { preventDefault: false },
+    });
+  }
+
+}
