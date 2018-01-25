b4656be0a13aba32082ca41b21e759617de96fc4
Alexandr Vorobyov <a.vorobyov@bvblogic.com>
2018-01-25 13:48:04 +0200

    * ICO Industry slug

--- api/models/IcoIndustry.js
+++ api/models/IcoIndustry.js
@@ -1,9 +1,4 @@
-/**
- * IcoIndustry.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
- */
+const slug = require('slug');
 
 module.exports = {
   attributes: {
