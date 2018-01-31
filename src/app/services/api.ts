c7b05f55d123f74548466ecdd98229d916cd5cc9
Ivan Yakivchuk <i.yakivchuk@bvblogic.com>
2018-01-31 18:52:10 +0200

    * Add request for check user auth token

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -8,9 +8,11 @@ import { Router } from '@angular/router';
 import { ApiHelperService } from './apiHelper';
 import { BroadcastService } from './broadcastService';
 
+
 @Injectable()
 export class ApiService {
   api_url = 'http://haumea.bvblogic.net:8103';
+  check_user = 'api/v1/user';
   signin_url = 'api/v1/signin';
   signup_url = 'api/v1/signup';
   activate_url = 'api/v1/activate?code=';
